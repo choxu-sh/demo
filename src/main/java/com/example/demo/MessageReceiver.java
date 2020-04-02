32a651c2863cd99da6f2c69d934fe1c9374f0868
bumpchong <bumpchong@yahoo.com>
2020-04-02 17:24:32 +0800

    Disabled onmessage

--- src/main/java/com/example/demo/MessageReceiver.java
+++ src/main/java/com/example/demo/MessageReceiver.java
@@ -11,20 +11,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class MessageReceiver {
 
 	static final Logger LOG = LoggerFactory.getLogger(MessageReceiver.class);
 
 	private static final String ORDER_RESPONSE_QUEUE = "domain.accountmanagement.orderLifeCycle.approved";
 
-	@JmsListener(destination = ORDER_RESPONSE_QUEUE)
+	/*@JmsListener(destination = ORDER_RESPONSE_QUEUE)
 	public void receiveMessage(final Map message) throws JMSException {
 		LOG.info("+++++++++++++++++++++++++++++++++++++++++++++++++++++");
 		LOG.info("orderId={}", message.get("orderId"));
 		LOG.info("trigger={}", message.get("trigger"));
 		LOG.info("afterStatus={}", message.get("trigger"));
 		LOG.info("+++++++++++++++++++++++++++++++++++++++++++++++++++++");
-	}
+	}*/
 
 }
