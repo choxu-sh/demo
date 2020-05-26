b912404b642f1e7b3d2d104f4558fec89c79b644
bumpchong <bumpchong@yahoo.com>
2020-05-26 15:31:48 +0800

    Updated code

--- src/main/java/com/example/demo/MessagingConfiguration.java
+++ src/main/java/com/example/demo/MessagingConfiguration.java
@@ -49,7 +49,7 @@ public class MessagingConfiguration {
 	
 	@Bean("senderConnectionFactorySSL")
 	public ActiveMQSslConnectionFactory connectionFactorySenderSSL() throws Exception {
-		String DEFAULT_BROKER_URL_SSL = "ssl://10.131.117.87:61620";
+		String DEFAULT_BROKER_URL_SSL = "ssl://dedicated-bus.mq.slcq045.com:61620";
 				
 				//"failover:(ssl://dedicated-bus.mq.slcq045.com:61620?wireFormat.maxInactivityDuration=0)?randomize=false&maxReconnectAttempts=5"; 
 				 //"failover:(tcp://"+senderBroker+":61616)?randomize=false&jms.redeliveryPolicy.maximumRedeliveries=99&jms.redeliveryPolicy.initialRedeliveryDelay=600000&jms.prefetchPolicy.all=1";
